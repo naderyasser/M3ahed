769248f54552d586c718c441f90f14f51860fbde
h4x0r <95550541+naderyasser@users.noreply.github.com>
2024-11-12 14:01:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -204,5 +204,5 @@ app.register_blueprint(admin)
 if __name__ == '__main__':
       with app.app_context():
             db.create_all()
-      app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=True)
+      app.run(debug=false, host='0.0.0.0', port=5000, use_reloader=True)
 
